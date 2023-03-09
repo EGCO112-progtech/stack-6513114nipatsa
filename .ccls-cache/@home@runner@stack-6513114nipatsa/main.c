6a613f4a0242de88920bed53dacca198e73bc4cc
6513114nipatsa <123149064+6513114nipatsa@users.noreply.github.com>
2023-03-09 10:11:32 +0000

    done

--- .ccls-cache/@home@runner@stack-6513114nipatsa/main.c
+++ .ccls-cache/@home@runner@stack-6513114nipatsa/main.c
@@ -8,63 +8,43 @@ void push(NodePtr* top,int x);
 int pop(NodePtr* top);
 */
 
-void checking(int error, int i)
-{
-  if (error == 0) {
-    printf("argv %d: incorrect: mismatch\n", i);
-  } else if (error == 2)
-    printf("argv %d: incorrect: too many open parenthesis\n", i);
-  else if (error == 3)
-    printf("argv %d: incorrect: too many closed parenthesis\n", i);
-  else
-    printf("argv %d: correct\n", i);
-}
 
 int main(int argc, char **argv){
- 
-  int i,N,j,error=1,counter = 0;;
+  
+  int i,j,error1=0,error2=0,error3=0;
   Stack s;
   s.top=NULL;
   s.size=0;
   
-  for(i=1;i<argc;i++){
-    error=1;
-    char k;
-    for(j=0;j<strlen(argv[i]);j++){
-     switch(argv[i][j]){  
-       case '{':
-       case '[':  push(&s,argv[i][j]); 
-                  break;
-     
-       case ']':   k = pop(&s);
-                  if (k == 'a')
-                   {
-                  error = 3;
-                  counter++;
-                   }
-        else if (k != '{') error = 0;
-        break;
-       case '}':  k = pop(&s);
-                  if (k == 'a')
-                  {
-                  error = 3;
-                  counter++;
-                  }
-        else if (k != '[') error= 0;
-        break;
-      //if(error==1) break;
-    }
- }
-    if (s.size - counter > 0) error = 2;
-    else if (s.size - counter < 0) error = 3;
-
-    checking(error, i);
-    pop_all(&s);
+  for(i=1;i<argc;i++)
+    {
+      for(j=0;j<strlen(argv[i]);j++)
+        {
+          switch(argv[i][j])
+          {
+            case '{':
+            case '[': push(&s,argv[i][j]); error1=1; break;
+            case '}': error2=1; if(pop(&s)!='{') error3=1; break;
+            case ']': error2=1; if(pop(&s)!='[') error3=1; break;
+          }
+        }    
+      
+      if(error1==1 && error2==1 && error3!=1)
+        printf("argv %d correct\n",i);
+      else if(error1==1 && error2==1 && error3==1)
+        printf("argv %d incorrect: mismatch\n",i);
+      else if(error1==1 && (error2==0 || error3==0))
+        printf("argv %d incorrect: too many open parenthesis\n",i);
+      else 
+        printf("argv %d incorrect: too many closed parenthesis\n",i);
+      error1=0;
+      error2=0;
+      error3=0;
+      pop_all(&s);
+  
+      }
+  return 0;
   }
-  pop_all(&s);
-   return 0;
-}
-
     
     /*if(s.size>0){
       printf("argv %d: Incorrect too many open parenthesis\n",i);
--- stack.h
+++ stack.h
@@ -6,33 +6,39 @@ typedef struct {
   int size;
 } Stack;
 
-typedef Stack *StackPtr;
-void push(StackPtr s, int value) {
-  NodePtr new_node = (NodePtr)malloc(sizeof(Node));
-  if (new_node) {
-    new_node->data = value;
-    new_node->nextPtr = s->top;
-    s->top = new_node;
+typedef Stack * StackPtr;
+void push(StackPtr s, char value)
+{
+  NodePtr new_node=(NodePtr)malloc(sizeof(Node));
+  if(new_node)
+  {
+    new_node->data=value;
+    new_node->nextPtr=s->top;
+    s->top=new_node;
     s->size++;
   }
 }
-char pop(StackPtr s) {
-  NodePtr t = s->top;
-  if (s->size > 0) {
-    char value = t->data; //(*top)->data
-    s->top = t->nextPtr; //(*top)->nextPtr
+
+int pop(StackPtr s)
+{
+  NodePtr temp = s->top;
+if(s->size>0)
+  {
+    int value=temp->data;
+    s->top=temp->nextPtr;
+    free(temp);
     s->size--;
-    free(t);
     return value;
   }
-  //printf("Empty stack");
-  return 0;
-}
+  //printf("Empty Stack\n");
+return 0;
+  }
 
-void pop_all(StackPtr s) {
-  while (s->size>0) 
-  pop(&s); //s->top
-   // printf("%d\n",pop(s));
-  
-}
+void pop_all(StackPtr s)
+{
+  while(s->size>0)
+    {
+      pop(s);
+    }
+  }
 #endif
